ee9a455bec3be5018126835cb68fe1073b36b8e5
DarwinFelzRodz <113707023+DarwinFelzRodz@users.noreply.github.com>
2023-04-24 09:17:04 -0400

    Add files via upload

---
+++ Programacion 2/buscar el nombre/buscar el hombre/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace buscar_el_nombre
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+
+
+            {
+                var listado = new[]
+                {
+                    new {Nombre= "Jose", Apellido="Manuel" },
+                    new {Nombre= "Mario", Apellido="Castaneda" },
+                    new {Nombre= "Marcos", Apellido="Valencia" },
+                    new {Nombre= "Ringo", Apellido="Martines"},
+                    new {Nombre= "Yocelin", Apellido="Santana" },
+                    new {Nombre= "Maria", Apellido="Matos"},
+                    new {Nombre= "Santos", Apellido="Escobar", },
+                    new {Nombre= "Pedro", Apellido="Valdez"},
+
+
+                };
+
+                XElement personas = new XElement("personas", // Este es la raiz
+                    from a in listado
+                    select new XElement("persona", new XAttribute("Nombre", a.Nombre),
+                    new XElement("Apellido", a.Apellido)
+
+                     )
+                     );
+
+                //TxtNombre.Text = personas.ToString();
+                //txtApellido.Text = personas.ToString();
+               
+                string[] Listado = { "Listado" };
+                
+                IEnumerable<string> encontrados = from l in Listado
+                                                  where l.Contains(TxtNombre.Text)
+                                                  orderby l
+                                                  select l;
+
+                //mostramos los resultados
+                foreach (string Nombre in encontrados)
+                    DTG.Text = Nombre;
+                //DTG = personas.ToString()
+
+
+                //int e = DTG.Rows.Add();
+                DTG.Rows[0].Cells[0].Value = new List<XElement>();
+                //    Console.WriteLine(Nombre);
+                //Console.WriteLine("_______");
+
+
+
+                //Console.WriteLine(personas);
+                personas.Save("Peronas.xml");
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+        }
+    }
+}
---
+++ Programacion 2/programa final/programa final/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace programa_final
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DTG = new System.Windows.Forms.DataGridView();
+            this.btnbuscar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DTG)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DTG
+            // 
+            this.DTG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DTG.Location = new System.Drawing.Point(29, 180);
+            this.DTG.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.DTG.Name = "DTG";
+            this.DTG.RowHeadersWidth = 51;
+            this.DTG.Size = new System.Drawing.Size(1008, 300);
+            this.DTG.TabIndex = 0;
+            // 
+            // btnbuscar
+            // 
+            this.btnbuscar.Font = new System.Drawing.Font("Showcard Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnbuscar.Location = new System.Drawing.Point(513, 26);
+            this.btnbuscar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnbuscar.Name = "btnbuscar";
+            this.btnbuscar.Size = new System.Drawing.Size(155, 52);
+            this.btnbuscar.TabIndex = 1;
+            this.btnbuscar.Text = "Buscar";
+            this.btnbuscar.UseVisualStyleBackColor = true;
+            this.btnbuscar.Click += new System.EventHandler(this.btnbuscar_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Showcard Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(513, 98);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 52);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // lbl1
+            // 
+            this.lbl1.Font = new System.Drawing.Font("Showcard Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(24, 26);
+            this.lbl1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(241, 28);
+            this.lbl1.TabIndex = 3;
+            this.lbl1.Text = "Dato";
+            this.lbl1.UseCompatibleTextRendering = true;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(29, 71);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNombre.Multiline = true;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(387, 56);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnbuscar);
+            this.Controls.Add(this.DTG);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DTG)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DTG;
+        private System.Windows.Forms.Button btnbuscar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox txtNombre;
+    }
+}
+
